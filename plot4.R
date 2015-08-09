efea154140191e18fe81d359fd1272c5c5668225
vp44 <vp44@cornell.edu>
2015-08-09 13:08:11 -0400

    small corrections

--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ with(data, {
   plot(Global_active_power~Datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
   plot(Voltage~Datetime, type="l", 
-       ylab="Voltage (volt)", xlab="")
+       ylab="Voltage (volt)", xlab="datetime")
   plot(Sub_metering_1~Datetime, type="l", 
        ylab="Global Active Power (kilowatts)", xlab="")
   lines(Sub_metering_2~Datetime,col='Red')
@@ -22,7 +22,7 @@ with(data, {
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
          legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.35)
   plot(Global_reactive_power~Datetime, type="l", 
-       ylab="Global Rective Power (kilowatts)",xlab="")
+       ylab="Global Rective Power (kilowatts)",xlab="datetime")
 })
 
 ## Saving to file
